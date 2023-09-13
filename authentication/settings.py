5b919f301775cad028dc74aea7621ba23d70f211
Adesina Abdulrahman <103801534+timmyades3@users.noreply.github.com>
2023-09-13 08:43:45 +0100

    Update settings.py

--- authentication/settings.py
+++ authentication/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://authentication-bhoe.onrender.com']
 
 
 # Application definition
