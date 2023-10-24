9287c847156ba52fa3afc81a0e99dbf89233fcd1
Adesina Abdulrahman <103801534+timmyades3@users.noreply.github.com>
2023-10-24 14:33:58 +0100

    Update settings.py

--- authentiaction/settings.py
+++ authentiaction/settings.py
@@ -80,9 +80,17 @@ WSGI_APPLICATION = 'authentiaction.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('PGDATABASE'),
+        'USER': config('PGUSER'),
+        'PASSWORD': config('PGPASSWORD'),
+        'HOST': config('PGHOST'),
+        'PORT': '5432',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
